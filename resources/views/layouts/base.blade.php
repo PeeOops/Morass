70848f03b8644a6e5212480708e1f8f9805fea55
PeeOops <liem.richiee@gmail.com>
2023-01-11 01:55:32 +0700

    Shop, Cart and Checkout Page Done

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -25,7 +25,7 @@
     <nav class="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
 
         <div class="container">
-            <a class="navbar-brand" href="index.html">Morass<span>.</span></a>
+            <a class="navbar-brand" href="/">Morass<span>.</span></a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni"
                 aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
@@ -34,19 +34,17 @@
 
             <div class="collapse navbar-collapse" id="navbarsFurni">
                 <ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="index.html">Home</a>
-                    </li>
-                    <li><a class="nav-link" href="shop.html">Shop</a></li>
+                    <li><a class="nav-link" href="/">Home</a></li>
+                    <li><a class="nav-link" href="/shop">Shop</a></li>
+                    <li><a class="nav-link" href="/checkout">Checkout</a></li>
                     <li><a class="nav-link" href="about.html">About us</a></li>
-                    <li><a class="nav-link" href="services.html">Services</a></li>
                     <li><a class="nav-link" href="blog.html">Blog</a></li>
                     <li><a class="nav-link" href="contact.html">Contact us</a></li>
                 </ul>
 
                 <ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
                     <li><a class="nav-link" href="#"><img src="{{ asset('images/user.svg') }}"></a></li>
-                    <li><a class="nav-link" href="cart.html"><img src="{{ asset('images/cart.svg') }}"></a></li>
+                    <li><a class="nav-link" href="/cart"><img src="{{ asset('images/cart.svg') }}"></a></li>
                 </ul>
             </div>
         </div>
@@ -54,11 +52,11 @@
     </nav>
     <!-- End Header/Navigation -->
 
-<!-- MAIN SECTION -->
+    <!-- MAIN SECTION -->
 
     {{$slot}}
 
-<!-- END OF MAIN SECTION -->
+    <!-- END OF MAIN SECTION -->
 
     <!-- Start Footer Section -->
     <footer class="footer-section">
@@ -71,8 +69,9 @@
             <div class="row">
                 <div class="col-lg-8">
                     <div class="subscription-form">
-                        <h3 class="d-flex align-items-center"><span class="me-1"><img src="{{ asset('images/envelope-outline.svg')}}"
-                                    alt="Image" class="img-fluid"></span><span>Subscribe to Newsletter</span></h3>
+                        <h3 class="d-flex align-items-center"><span class="me-1"><img
+                                    src="{{ asset('images/envelope-outline.svg')}}" alt="Image"
+                                    class="img-fluid"></span><span>Subscribe to Newsletter</span></h3>
 
                         <form action="#" class="row g-3">
                             <div class="col-auto">
